212def7e345826b37e6508f2f11edcf09671a8b6
Denis Ivanchenko <sicknature24@yandex.com>
2023-05-10 14:31:15 +0300

    feat: add game information constants; nested functions for creating layout

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -11,10 +11,24 @@ const GAME_DIFFICULTIES = {
     }
 }
 
-const appendGameBoardElement = () => {
-    const GAME_BOARD_ELEMENT = document.createElement('div');
-    GAME_BOARD_ELEMENT.classList.add('game-board');
-    document.body.appendChild(GAME_BOARD_ELEMENT);
+const createBasicLayout = () => {
+    const appendGameBoardElement = () => {
+        const GAME_BOARD_ELEMENT = document.createElement('div');
+        GAME_BOARD_ELEMENT.classList.add('game-board');
+        document.body.appendChild(GAME_BOARD_ELEMENT);
+    }
+    const appendGameInformation = () => {
+        const GAME_INFORMATION = document.createElement('div');
+
+        const GAME_BOARD_THEME_BUTTON = document.createElement('button');
+        const TOTAL_BOMBS_ON_GAME_BOARD = document.createElement('span');
+        const NUMBER_OF_CLICKS = document.createElement('span');
+        const GAME_DURATION = document.createElement('span');
+        const GAME_RESTART_BUTTON = document.createElement('button');
+        const NUMBER_OF_FLAGS_REMAINED = document.createElement('span');
+    }
+    appendGameBoardElement();
+    appendGameInformation();
 }
 
 const createGameBoard = (setRows, setColumns) => {
@@ -28,9 +42,7 @@ const createGameBoard = (setRows, setColumns) => {
     }
 }
 
-const initGame = () => {
-    appendGameBoardElement();
+(function initGame() {
+    createBasicLayout();
     createGameBoard(GAME_DIFFICULTIES.easy.rows,GAME_DIFFICULTIES.easy.columns);
-}
-
-initGame();
+}());
