2c96b2b5f66bfdff6479f3d1df168e802008460a
Denis Ivanchenko <sicknature24@yandex.com>
2023-05-11 14:33:20 +0300

    feat: add handleBarClick function; styles for new html elements

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -1,6 +1,5 @@
 const gameBoard = [];
 const mines = [];
-let barsClicked = 0;
 let isGameOver = false;
 
 const GAME_DIFFICULTIES = {
@@ -11,6 +10,14 @@ const GAME_DIFFICULTIES = {
         flags: 10,
     }
 }
+
+mines.push('1-1');
+mines.push('2-2');
+mines.push('3-3');
+mines.push('4-4');
+mines.push('5-5');
+
+// todo: 1. Restart button; 2. Game duration timer; 3. Webpack?
 
 const createBasicLayout = () => {
     const appendGameBoardElement = () => {
@@ -24,19 +31,37 @@ const createBasicLayout = () => {
 
         const NUMBER_OF_FLAGS_TEXT = document.createElement('div');
         NUMBER_OF_FLAGS_TEXT.classList.add('red-flags-information');
-        NUMBER_OF_FLAGS_TEXT.innerText = 'Number of 🚩 remained:';
+        NUMBER_OF_FLAGS_TEXT.innerText = 'Number of 🚩 remained: ';
 
         const NUMBER_OF_FLAGS = document.createElement('span');
         NUMBER_OF_FLAGS.classList.add('red-flags-number');
         NUMBER_OF_FLAGS.innerText = GAME_DIFFICULTIES.easy.flags;
 
-        const GAME_BOARD_THEME_BUTTON = document.createElement('button');
-        const TOTAL_BOMBS_ON_GAME_BOARD = document.createElement('span');
+        const TOTAL_BOMBS_ON_GAME_BOARD = document.createElement('div');
+        TOTAL_BOMBS_ON_GAME_BOARD.classList.add('bombs-number');
+        TOTAL_BOMBS_ON_GAME_BOARD.innerText = `Number of 💣 on the board: ${GAME_DIFFICULTIES.easy.bombs}`;
+
+        const NUMBER_OF_CLICKS_TEXT = document.createElement('div');
+        NUMBER_OF_CLICKS_TEXT.classList.add('clicks-number-information');
+        NUMBER_OF_CLICKS_TEXT.innerText = 'Number of clicks: ';
+
         const NUMBER_OF_CLICKS = document.createElement('span');
-        const GAME_DURATION = document.createElement('span');
+        NUMBER_OF_CLICKS.classList.add('clicks-number');
+        NUMBER_OF_CLICKS.innerText = '0';
+
         const GAME_RESTART_BUTTON = document.createElement('button');
+        GAME_RESTART_BUTTON.classList.add('restart-button');
+        GAME_RESTART_BUTTON.innerText = 'Restart Game'
+
+
+        const GAME_BOARD_THEME_BUTTON = document.createElement('button');
+        const GAME_DURATION = document.createElement('span');
 
         GAME_INFORMATION.appendChild(NUMBER_OF_FLAGS_TEXT);
+        GAME_INFORMATION.appendChild(TOTAL_BOMBS_ON_GAME_BOARD);
+        GAME_INFORMATION.appendChild(NUMBER_OF_CLICKS_TEXT);
+        GAME_INFORMATION.appendChild(GAME_RESTART_BUTTON);
+        NUMBER_OF_CLICKS_TEXT.appendChild(NUMBER_OF_CLICKS);
         NUMBER_OF_FLAGS_TEXT.appendChild(NUMBER_OF_FLAGS);
         document.body.appendChild(GAME_INFORMATION);
     }
@@ -57,6 +82,41 @@ const createGameBoard = (setRows, setColumns) => {
         gameBoard.push(boardRow);
     }
     console.log(gameBoard);
+}
+
+const handleBarClick = () => {
+    const BOARD_BARS = document.querySelectorAll('.bar');
+    const clicksCounter = document.querySelector('.clicks-number');
+
+    BOARD_BARS.forEach((bar) => {
+        bar.addEventListener('click', () => {
+            if (isGameOver === false) {
+                clicksCounter.innerText = parseInt(clicksCounter.innerText) + 1;
+            }
+            console.log(bar.classList);
+            if(mines.includes(bar.classList[0])) {
+                alert('BRO YOU\'VE JUST CLICKED ON THE BOMB');
+                isGameOver = true;
+                showMines();
+            }
+        })
+    })
+}
+
+const showMines = () => {
+    const BOARD_BARS = document.querySelectorAll('.bar');
+    BOARD_BARS.forEach((bar) => {
+        if (mines.includes(bar.classList[0])) {
+            bar.innerText = '💣';
+            bar.classList.add('bomb');
+        }
+    })
+}
+
+const restartGame = () => {
+    if (isGameOver === true) {
+
+    }
 }
 
 const handleRedFlag = () => {
@@ -85,5 +145,6 @@ const handleRedFlag = () => {
 (function initGame() {
     createBasicLayout();
     createGameBoard(GAME_DIFFICULTIES.easy.rows,GAME_DIFFICULTIES.easy.columns);
+    handleBarClick();
     handleRedFlag();
 }());
