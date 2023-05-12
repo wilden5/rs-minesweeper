8dd650632e074ed952f8ae086b1773c5f5b61b20
Denis Ivanchenko <sicknature24@yandex.com>
2023-05-12 13:20:58 +0300

    feat: add stopwatch function; split init function to game and layout functions

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -17,7 +17,8 @@ mines.push('3-3');
 mines.push('4-4');
 mines.push('5-5');
 
-// todo: 1. Restart button; 2. Game duration timer; 3. Webpack?
+// todo: 1. Remove click if user marked bar with a flag; 2. Bombs indicator should connects to flags? 3. Webpack?
+// todo: 4. Increase counter when contextclick?
 
 const createBasicLayout = () => {
     const appendGameBoardElement = () => {
@@ -51,22 +52,55 @@ const createBasicLayout = () => {
 
         const GAME_RESTART_BUTTON = document.createElement('button');
         GAME_RESTART_BUTTON.classList.add('restart-button');
-        GAME_RESTART_BUTTON.innerText = 'Restart Game'
+        GAME_RESTART_BUTTON.innerText = 'Restart Game';
+
+        const GAME_DURATION = document.createElement('div');
+        GAME_DURATION.classList.add('game-duration');
+        GAME_DURATION.innerText = 'Duration of the game: ';
+
+        const GAME_TIMER = document.createElement('span');
+        GAME_TIMER.classList.add('timer-seconds');
+        GAME_TIMER.innerText = '0';
+
+        const START_GAME_BUTTON = document.createElement('button');
+        START_GAME_BUTTON.classList.add('start-game-button');
+        START_GAME_BUTTON.innerText = 'START A NEW GAME';
+
 
 
         const GAME_BOARD_THEME_BUTTON = document.createElement('button');
-        const GAME_DURATION = document.createElement('span');
 
         GAME_INFORMATION.appendChild(NUMBER_OF_FLAGS_TEXT);
         GAME_INFORMATION.appendChild(TOTAL_BOMBS_ON_GAME_BOARD);
         GAME_INFORMATION.appendChild(NUMBER_OF_CLICKS_TEXT);
+        GAME_INFORMATION.appendChild(GAME_DURATION);
         GAME_INFORMATION.appendChild(GAME_RESTART_BUTTON);
+        GAME_INFORMATION.appendChild(START_GAME_BUTTON);
         NUMBER_OF_CLICKS_TEXT.appendChild(NUMBER_OF_CLICKS);
         NUMBER_OF_FLAGS_TEXT.appendChild(NUMBER_OF_FLAGS);
+        GAME_DURATION.appendChild(GAME_TIMER);
         document.body.appendChild(GAME_INFORMATION);
     }
     appendGameInformation();
     appendGameBoardElement();
+}
+
+
+let timerInterval;
+
+function startTimer() {
+    const GAME_TIMER = document.querySelector('.timer-seconds');
+    let startTime = Date.now();
+    let elapsedTime = 0;
+
+    timerInterval = setInterval(function() {
+        elapsedTime = Math.floor((Date.now() - startTime) / 1000);
+        GAME_TIMER.innerText = elapsedTime;
+    }, 100);
+}
+
+function stopTimer() {
+    clearInterval(timerInterval);
 }
 
 const createGameBoard = (setRows, setColumns) => {
@@ -94,10 +128,11 @@ const handleBarClick = () => {
                 clicksCounter.innerText = parseInt(clicksCounter.innerText) + 1;
             }
             console.log(bar.classList);
-            if(mines.includes(bar.classList[0])) {
-                alert('BRO YOU\'VE JUST CLICKED ON THE BOMB');
+            if (mines.includes(bar.classList[0])) {
+                alert('BRO YOU\'VE JUST CLICKED ON THE BOMB. GAME OVER. TRY AGAIN.');
                 isGameOver = true;
                 showMines();
+                stopTimer();
             }
         })
     })
@@ -114,9 +149,12 @@ const showMines = () => {
 }
 
 const restartGame = () => {
-    if (isGameOver === true) {
-
-    }
+    const RESTART_BUTTON = document.querySelector('.restart-button');
+    RESTART_BUTTON.addEventListener('click', () => {
+        isGameOver = false;
+        document.body.innerHTML = '';
+        createLayout();
+    })
 }
 
 const handleRedFlag = () => {
@@ -125,8 +163,9 @@ const handleRedFlag = () => {
     const RED_FLAGS_NUMBER = document.querySelector('.red-flags-number');
 
     BOARD_BARS.forEach((bar) => {
-            bar.addEventListener('contextmenu', (event) => {
-                event.preventDefault();
+        bar.addEventListener('contextmenu', (event) => {
+            event.preventDefault();
+            if(isGameOver === false) {
                 if (bar.innerHTML === '') {
                     if (RED_FLAGS_NUMBER.innerText > 0) {
                         RED_FLAGS_NUMBER.innerText = parseInt(RED_FLAGS_NUMBER.innerText) - 1;
@@ -138,13 +177,29 @@ const handleRedFlag = () => {
                     bar.innerHTML = '';
                     bar.classList.toggle('red-flag');
                 }
-            });
+            }
+        });
     });
 }
 
-(function initGame() {
+const createLayout = () => {
     createBasicLayout();
-    createGameBoard(GAME_DIFFICULTIES.easy.rows,GAME_DIFFICULTIES.easy.columns);
-    handleBarClick();
-    handleRedFlag();
-}());
+    createGameBoard(GAME_DIFFICULTIES.easy.rows, GAME_DIFFICULTIES.easy.columns);
+};
+
+const startGameSession = () => {
+    const START_GAME_BUTTON = document.querySelector('.start-game-button');
+    START_GAME_BUTTON.addEventListener('click', () => {
+        START_GAME_BUTTON.disabled = true;
+        startTimer();
+        handleBarClick();
+        handleRedFlag();
+    })
+}
+
+const init = () => {
+    createLayout();
+    startGameSession();
+}
+
+init();
