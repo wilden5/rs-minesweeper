8b8312a9d74fc2995a6cd0e6ab79e24a51a0b804
Denis Ivanchenko <sicknature24@yandex.com>
2023-05-13 14:10:01 +0300

    feat: add generic createElement function to reduce code lines

--- minesweeper/src/modules/gameLayout.js
+++ minesweeper/src/modules/gameLayout.js
@@ -2,76 +2,42 @@ import GAME_DIFFICULTIES from '../data/difficulties.json';
 
 const gameBoard = [];
 
+const createElement = (type, classes = [], text = '') => {
+    const element = document.createElement(type);
+    classes.forEach(className => element.classList.add(className));
+    if (text) {
+        element.innerText = text;
+    }
+    return element;
+}
+
 export const createBasicLayout = () => {
-    const appendGameBoardElement = () => {
-        const GAME_BOARD_ELEMENT = document.createElement('div');
-        GAME_BOARD_ELEMENT.classList.add('game-board');
-        document.body.appendChild(GAME_BOARD_ELEMENT);
-    }
+    const GAME_BOARD_ELEMENT = createElement('div', ['game-board']);
+    const NUMBER_OF_FLAGS_TEXT = createElement('div', ['red-flags-information'], 'Number of 🚩 used: ');
+    const NUMBER_OF_FLAGS = createElement('span', ['red-flags-number'], '0');
+    const TOTAL_BOMBS_ON_GAME_BOARD = createElement('div', ['bombs-number'], 'Number of 💣 remaining: ');
+    const BOMBS_REMAINED = createElement('span', ['bombs-remained'], GAME_DIFFICULTIES.easy.bombs.toString());
+    const NUMBER_OF_CLICKS_TEXT = createElement('div', ['clicks-number-information'], 'Number of clicks: ');
+    const NUMBER_OF_CLICKS = createElement('span', ['clicks-number'], '0');
+    const GAME_RESTART_BUTTON = createElement('button', ['restart-button'], 'Restart Game');
+    const GAME_DURATION = createElement('div', ['game-duration'], 'Duration of the game: ');
+    const GAME_TIMER = createElement('span', ['timer-seconds'], '0');
+    const START_GAME_BUTTON = createElement('button', ['start-game-button'], 'START A NEW GAME');
+    const GAME_INFORMATION = createElement('div', ['game-information']);
 
-    const appendGameInformation = () => {
-        const GAME_INFORMATION = document.createElement('div');
-        GAME_INFORMATION.classList.add('game-information')
+    GAME_INFORMATION.appendChild(NUMBER_OF_FLAGS_TEXT);
+    GAME_INFORMATION.appendChild(TOTAL_BOMBS_ON_GAME_BOARD);
+    GAME_INFORMATION.appendChild(NUMBER_OF_CLICKS_TEXT);
+    GAME_INFORMATION.appendChild(GAME_DURATION);
+    GAME_INFORMATION.appendChild(GAME_RESTART_BUTTON);
+    GAME_INFORMATION.appendChild(START_GAME_BUTTON);
+    TOTAL_BOMBS_ON_GAME_BOARD.appendChild(BOMBS_REMAINED);
+    NUMBER_OF_CLICKS_TEXT.appendChild(NUMBER_OF_CLICKS);
+    NUMBER_OF_FLAGS_TEXT.appendChild(NUMBER_OF_FLAGS);
+    GAME_DURATION.appendChild(GAME_TIMER);
 
-        const NUMBER_OF_FLAGS_TEXT = document.createElement('div');
-        NUMBER_OF_FLAGS_TEXT.classList.add('red-flags-information');
-        NUMBER_OF_FLAGS_TEXT.innerText = 'Number of 🚩 used: ';
-
-        const NUMBER_OF_FLAGS = document.createElement('span');
-        NUMBER_OF_FLAGS.classList.add('red-flags-number');
-        NUMBER_OF_FLAGS.innerText = '0';
-
-        const TOTAL_BOMBS_ON_GAME_BOARD = document.createElement('div');
-        TOTAL_BOMBS_ON_GAME_BOARD.classList.add('bombs-number');
-        TOTAL_BOMBS_ON_GAME_BOARD.innerText = 'Number of 💣 remaining: ';
-
-        const BOMBS_REMAINED = document.createElement('span');
-        BOMBS_REMAINED.classList.add('bombs-remained');
-        BOMBS_REMAINED.innerText = GAME_DIFFICULTIES.easy.bombs;
-
-        const NUMBER_OF_CLICKS_TEXT = document.createElement('div');
-        NUMBER_OF_CLICKS_TEXT.classList.add('clicks-number-information');
-        NUMBER_OF_CLICKS_TEXT.innerText = 'Number of clicks: ';
-
-        const NUMBER_OF_CLICKS = document.createElement('span');
-        NUMBER_OF_CLICKS.classList.add('clicks-number');
-        NUMBER_OF_CLICKS.innerText = '0';
-
-        const GAME_RESTART_BUTTON = document.createElement('button');
-        GAME_RESTART_BUTTON.classList.add('restart-button');
-        GAME_RESTART_BUTTON.innerText = 'Restart Game';
-
-        const GAME_DURATION = document.createElement('div');
-        GAME_DURATION.classList.add('game-duration');
-        GAME_DURATION.innerText = 'Duration of the game: ';
-
-        const GAME_TIMER = document.createElement('span');
-        GAME_TIMER.classList.add('timer-seconds');
-        GAME_TIMER.innerText = '0';
-
-        const START_GAME_BUTTON = document.createElement('button');
-        START_GAME_BUTTON.classList.add('start-game-button');
-        START_GAME_BUTTON.innerText = 'START A NEW GAME';
-
-
-
-        const GAME_BOARD_THEME_BUTTON = document.createElement('button');
-
-        GAME_INFORMATION.appendChild(NUMBER_OF_FLAGS_TEXT);
-        GAME_INFORMATION.appendChild(TOTAL_BOMBS_ON_GAME_BOARD);
-        GAME_INFORMATION.appendChild(NUMBER_OF_CLICKS_TEXT);
-        GAME_INFORMATION.appendChild(GAME_DURATION);
-        GAME_INFORMATION.appendChild(GAME_RESTART_BUTTON);
-        GAME_INFORMATION.appendChild(START_GAME_BUTTON);
-        TOTAL_BOMBS_ON_GAME_BOARD.appendChild(BOMBS_REMAINED);
-        NUMBER_OF_CLICKS_TEXT.appendChild(NUMBER_OF_CLICKS);
-        NUMBER_OF_FLAGS_TEXT.appendChild(NUMBER_OF_FLAGS);
-        GAME_DURATION.appendChild(GAME_TIMER);
-        document.body.appendChild(GAME_INFORMATION);
-    }
-
-    appendGameInformation();
-    appendGameBoardElement();
+    document.body.appendChild(GAME_INFORMATION);
+    document.body.appendChild(GAME_BOARD_ELEMENT);
 }
 
 export const createGameBoard = (setRows, setColumns) => {
@@ -79,8 +45,7 @@ export const createGameBoard = (setRows, setColumns) => {
     for (let row = 0; row < setRows; row++) {
         let boardRow = [];
         for (let column = 0; column < setColumns; column++) {
-            let bar = document.createElement('div');
-            bar.classList.add(`${row.toString()}-${column.toString()}`, 'bar');
+            let bar = createElement('div', [`${row.toString()}-${column.toString()}`, 'bar'])
             GAME_BOARD.appendChild(bar);
             boardRow.push(bar);
         }
