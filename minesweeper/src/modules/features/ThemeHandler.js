e0071b5cad791e15c2198e22349962cd0971553a
Denis Ivanchenko <sicknature24@yandex.com>
2023-05-22 14:06:40 +0300

    feat: add dark styles for mines button; board size buttons

--- minesweeper/src/modules/features/ThemeHandler.js
+++ minesweeper/src/modules/features/ThemeHandler.js
@@ -20,6 +20,8 @@ export const changeTheme = (color) => {
     const DARK_THEME_BUTTON = document.querySelector('.dark-theme');
     const LIGHT_THEME_BUTTON = document.querySelector('.light-theme');
     const SCORE_TABLE = document.querySelector('.score-table');
+    const MINES_SETTING_BUTTON = document.querySelector('.mines-settings-button');
+    const FIELD_BUTTONS = document.querySelectorAll('.field-button');
 
     if (color === 'dark') {
         BODY.classList.add('body-dark');
@@ -28,10 +30,15 @@ export const changeTheme = (color) => {
         DARK_THEME_BUTTON.classList.add('dark-theme-d');
         LIGHT_THEME_BUTTON.classList.add('light-theme-d');
         SCORE_TABLE.classList.add('score-table-dark');
+        MINES_SETTING_BUTTON.classList.add('mines-settings-button-dark');
 
         SESSION_BUTTONS.forEach(button => {
             button.classList.add('button-dark');
         });
+
+        FIELD_BUTTONS.forEach((button) => {
+            button.classList.add('field-button-dark');
+        })
     } else {
         BODY.classList.remove('body-dark');
         GAME_BOARD.classList.remove('game-board-dark');
@@ -39,9 +46,14 @@ export const changeTheme = (color) => {
         DARK_THEME_BUTTON.classList.remove('dark-theme-d');
         LIGHT_THEME_BUTTON.classList.remove('light-theme-d');
         SCORE_TABLE.classList.remove('score-table-dark');
+        MINES_SETTING_BUTTON.classList.remove('mines-settings-button-dark');
 
         SESSION_BUTTONS.forEach(button => {
             button.classList.remove('button-dark');
         });
+
+        FIELD_BUTTONS.forEach((button) => {
+            button.classList.remove('field-button-dark');
+        })
     }
 }
